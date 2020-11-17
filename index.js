1e118dc2e83da65cb6ad3159741d4d7f584c2033
KaleevaAlina <69644331+KaleevaAlina@users.noreply.github.com>
2020-11-17 19:13:05 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,75 @@
+"use strict"
+
+//1 пункт
+  /* let num=3;
+  alert (num) ; */ 
+
+//2 пункт 
+   /*  let a=10;
+    let b=2;
+    console.log (a+b);
+   
+    console.log (a-b);
+
+   
+    console.log (a*b);
+
+  
+   console.log (a/b);  */
+
+//3 пункт
+   /*  let c=15;
+    let d=2; 
+    let result = c+d;
+    // console.log (result); */
+
+//4 пункт 
+    /* let a=10;
+    let b=2;
+    let c=5;
+    console.log (a+b+c); */
+
+    /* function sum(a,b,c) {
+        return a+b+c;
+    }
+
+    console.log(sum(10,2,5)); */
+
+//5 пункт
+/*    let a = 17;
+    let b = 10;
+    let c = a-b;
+    let d = 7;
+    let result = c+d;
+    console.log (result);
+ */
+//6 пункт
+   /*  let str = Привет, Мир!;
+    console.log (str); */
+
+  /*   let str= `fullname: ${Привет,} ${Мир!} `
+    console.log (str);
+ */
+//7 пункт
+   /*  let str1 ='Привет,'
+    let str2 ='Мир!'
+    let result=str1+ ' ' + str2;
+    console.log (result); */ 
+// 8 пункт 
+   /*  let name = '%Alina%';
+    let result = 'Привет, '+name+'!';
+    console.log(result);
+ */
+
+//9 пункт
+    /* let str ='Мне'
+    let age ='19 лет'
+    let result=str+ ' ' + age+'!';
+    console.log (result);  */
+
+    /*  function sayYes(age) {
+        let me='Мне';
+        let str = me+ ' '+age+ ' лет!';
+        return str;
+        }
+       console.log(sayYes(19)); */
